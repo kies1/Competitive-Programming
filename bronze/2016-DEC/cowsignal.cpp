c2601a499e76bbddb5b3042d42019cd56ede35f9
tshu428 <106455819+tshu428@users.noreply.github.com>
2022-06-09 21:13:45 -0700

    Create cowsignal.cpp

---
+++ bronze/2016-DEC/cowsignal.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("cowsignal.in", "r", stdin);
+	freopen("cowsignal.out", "w", stdout);
+    int m,n,k;
+    cin>>m>>n>>k;
+    for(int i = 0; i < m;i++){
+        string s; cin >>s;
+        for(int y = 0; y<k;y++){
+            for(int j = 0; j<n; j++){
+            for(int x = 0; x<k;x++){
+                cout<<s[j];
+            }
+        }
+        cout<<endl;
+        }
+    }
+}
