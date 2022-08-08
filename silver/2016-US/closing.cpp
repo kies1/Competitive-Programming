9445d5b96105ca35f8e90eccb28821a8e2a4fee7
tshu428 <106455819+tshu428@users.noreply.github.com>
2022-08-07 17:53:06 -0700

    dfs

--- silver/2016-US/closing.cpp
+++ silver/2016-US/closing.cpp
@@ -1,62 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int MAX_N = 3002;
+constexpr int MN = 3002;
+vector<vector<int>> adj(MN);
+vector<bool> visited(MN,false);
+vector<bool> closed(MN,false);
+vector<int> order(MN);
+int nodeCnt = 0;
 
-vector<vector<int>> adj(MAX_N);
-vector<int> visited(MAX_N);
-vector<int> closed(MAX_N);
-int nodes = 0;
+void dfs (int node){
 
+	if(closed[node] || visited[node]) return;
 
-void dfs(int node) {
-	if (visited[node] || closed[node]) return;
+	visited[node]=true;\
+	nodeCnt++;
 
-	nodes++;
-	visited[node] = true;
-	
-	for (int u : adj[node]) {
-		if (!visited[u]) dfs(u);
+	for(auto child: adj[node]){
+		if(visited[child]==false){
+			dfs(child);
+		}
 	}
 }
 
 int main() {
+	ios::sync_with_stdio(false);
+        cin.tie(nullptr);
 	freopen("closing.in", "r", stdin);
 	freopen("closing.out", "w", stdout);
-	int n,m;
-	cin >> n >> m;
+	int n, m;
+	cin>>n>>m;
 
-	for (int i = 0; i < m; i++) {
+	for(int i = 0; i < m;i++){
 		int a, b;
-		cin >> a >> b;
+		cin>>a>>b;
 		adj[a].push_back(b);
 		adj[b].push_back(a);
-	}
-	
-	vector<int> order(MAX_N);
-	for (int i = 0; i < n; i++){
-		int x; cin>>x;
-		order[i]=x;
 	}
 
 	dfs(1);
 
-	if (nodes == n) {
-		cout << "YES"<<endl;
-	} else {
-		cout << "NO"<<endl;
-	}
-	
-	for (int i = 0; i < n - 1; i++) {
-		nodes = 0;
-		closed[order[i]] = true;
-		fill(visited.begin(), visited.end(), false);
+	if(nodeCnt==n)cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
 
-		dfs(order[n - 1]);
-		if (nodes == n - i - 1) {
-			cout << "YES" << endl;
-		} else {
-			cout << "NO" << endl;
+	for(int i = 0; i <n; i++){
+		cin>>order[i];
+	}
+
+	for(int i = 0; i < n-1; i++){
+		fill(visited.begin(),visited.end(),false);
+		nodeCnt=0;
+		closed[order[i]]=true;
+		dfs(order[n-1]);
+		if(nodeCnt==n-i-1){
+			cout<<"YES"<<endl;
+		}else{
+			cout<<"NO"<<endl;
 		}
 	}
 }
